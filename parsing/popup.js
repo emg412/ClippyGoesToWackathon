e639c8b63352f97ea6ad778562b6e37328966b29
Oliver Wrigley <oliverwrigley@Olivers-MacBook-Pro.local>
2019-02-16 15:54:36 -0500

    Removed button

--- parsing/popup.js
+++ parsing/popup.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    var resultsButton = document.getElementById("getResults");
-    resultsButton.onclick = getResults;
+    getResults();
 });
 
 function getResults(){
