f9b53ecb13e717ebaf24e72553303b73250c0cca
Pinksys <raminzarf@gmail.com>
2019-02-16 17:14:27 -0500

    testing storing and recalling from chrome storage

--- foodContent.js
+++ foodContent.js
@@ -1,1 +1,15 @@
-Console.log("foodContent.js ran");
+console.log("foodContent.js ran");
+
+var testKey = "theTestKey";
+var testVal = "myValueeee";
+
+chrome.storage.sync.set({testKey, testVal}, function() {
+	console.log("1) value stored: " + testVal);
+});
+
+var asdf = chrome.storage.sync.get(["theTestKey"], function() {
+	console.log("2) value retrieved: " + value);
+});
+
+console.log("3) retrieved value asdf: " + asdf);
+
