fde186e23d4faeea2a081d5428b5e447a6615df1
Pinksys <raminzarf@gmail.com>
2019-02-16 16:17:00 -0500

    generalContent and foodContent now have a single console.log statement

--- generalContent.js
+++ generalContent.js
@@ -1,13 +1,1 @@
-chrome.tabs.onUpdated.addListener(function({
-	//check if we are on a page where money can be spent
-	
-	chrome.pageCapture.saveAsMHTML(chrome.tabs.id, function(binary blob) {
-		var reader = new FileReader();
-		reader.onload = function() {
-    			alert(reader.result);
-		}
-		reader.readAsText(blob);
-		var text = reader.result;
-		
-	});
-}));
+Console.log("generalContent.js ran");
