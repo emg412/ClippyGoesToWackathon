7071f9c1cdf583b2d0ae3fcb6d23f865e69473b3
emg412 <emg412@vt.edu>
2019-02-16 19:31:33 -0500

    new files

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onInstalled.addListener(function(){
 	alert('Thank you for installing Peppy!\nYou will now be redirected to our home page where you can set up your account!\n');
 	chrome.tabs.create({
-		url:"place url for settings page here"
+		url:"chrome-extension://bkppmaacdeffhkhnhnpenmlphlegmkdo/localPage.html"
 		active: true
 	});
 });
