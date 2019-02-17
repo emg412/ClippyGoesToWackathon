2638ad8143c0395034948da6c2c896a22de8b3d9
Oliver Wrigley <oliverwrigley@Olivers-MacBook-Pro.local>
2019-02-17 00:53:55 -0500

    Updated

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     var resultsButton = document.getElementById("settings");
     resultsButton.onclick = settings;
     getResults();
+    showFood();
 });
 
 function getResults(){
@@ -15,6 +16,15 @@ function getResults(){
 function showResults(results) {
     var resultsElement = document.getElementById("results");
     resultsElement.innerText = results ? "This is a checkout cart." : "This is NOT a checkout cart.";
+}
+
+function showFood() {
+    var foodBal = localStorage.getItem("totalGeneralBudget");
+    var goodcashElement = document.getElementById("cash_good");
+    if (foodBal.length < 4) {
+        foodBal = foodBal + ".00"
+    }
+    goodcashElement.innerText = "$" + foodBal
 }
 
 function settings() {
