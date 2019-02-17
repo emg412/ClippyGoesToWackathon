3bd32aacae966d3874ffc9462a6565c13276ebf4
emg412 <emg412@vt.edu>
2019-02-16 23:01:30 -0500

    WE ARE STILL WINNERS

--- popup2.js
+++ popup2.js
@@ -1,6 +1,30 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    var resultsButton = document.getElementById("theButton").addEventListener('click', main);
+    var resultsButton = document.getElementById("theButton").addEventListener('click', userInfo);
 });
-function main(){
-	console.log("i wanna die");
+function userInfo(){
+	var firstName = document.getElementById('box1').value;
+	var userName = document.getElementById('box2').value;
+	var password = document.getElementById('box3').value;
+	localStorage.setItem("first", firstName);
+	localStorage.setItem("user", userName);
+	localStorage.setItem("pass", password);
+
+	console.log(localStorage.getItem("first"));
+	console.log(localStorage.getItem("user"));
+	console.log(localStorage.getItem("pass"));
+
+	window.open("categories.html");
+	window.close();
+
 }
+
+function budgetInfo(){
+	var clothingBudget = document.getElementById('box1').value; 	
+	var foodBudget = document.getElementById('box2').value;
+	var generalBudget = document.getElementById('box3').value;
+	localStorage.setItem("clothing", clothing);
+	localStorage.setItem("food", food);
+	localStorage.setItem("general", general);
+}
+
+
