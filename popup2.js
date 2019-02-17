ccf1f22bb91e37185adac124c189a9a1fbec341e
emg412 <emg412@vt.edu>
2019-02-16 22:08:14 -0500

    we are WINNERS

---
+++ popup2.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var resultsButton = document.getElementById("theButton").addEventListener('click', main);
+});
+function main(){
+	console.log("i wanna die");
+}
